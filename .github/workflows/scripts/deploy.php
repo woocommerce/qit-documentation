2089817fae7a8a0351cca9b70020b2d48e4aab39
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-03-20 18:29:12 -0300

    Fix

--- .github/workflows/scripts/deploy.php
+++ .github/workflows/scripts/deploy.php
@@ -22,7 +22,7 @@ if ( ! file_exists( getenv( 'FILE' ) ) ) {
 
 $file = new SplFileObject( getenv( 'FILE' ) );
 
-$chunk_size_bytes = 1024 * 1024; // 1mb
+$chunk_size_bytes = 8 * 1024 * 1024; // 8mb
 $current_chunk    = 0;
 $docs_upload_id   = wp_generate_uuid4();
 $total_chunks     = ceil( $file->getSize() / ( $chunk_size_bytes ) );
@@ -31,7 +31,7 @@ while ( $file->valid() ) {
 	$current_chunk ++;
 	$curl = curl_init();
 	$args = [
-		CURLOPT_URL            => 'https://stagingcompatibilitydashboard.wpcomstaging.com/wp-content/plugins/deploy-docs.php',
+		CURLOPT_URL            => 'https://stagingcompatibilitydashboard.wpcomstaging.com',
 		CURLOPT_POST           => true,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_FOLLOWLOCATION => true,
