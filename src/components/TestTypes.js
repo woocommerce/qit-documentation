5b2deb2360989629222b06a5ed0c30b812c754ca
Benedict Singer <singerb@mac.com>
2024-08-26 14:58:09 -0400

    Add initial shell of validation test documentation

--- src/components/TestTypes.js
+++ src/components/TestTypes.js
@@ -9,19 +9,21 @@ export default function TestTypes({ includeCode = false }) {
                 Managed Tests
                 <ul>
                     <li><a href="/docs/managed-tests/woo-e2e">Woo E2E Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:woo-e2e</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:woo-e2e</code></li>
                     <li><a href="/docs/managed-tests/woo-api">Woo API Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:woo-api</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:woo-api</code></li>
                     <li><a href="/docs/managed-tests/activation">Activation Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:activation</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:activation</code></li>
                     <li><a href="/docs/managed-tests/security">Security Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:security</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:security</code></li>
                     <li><a href="/docs/managed-tests/phpstan">PHPStan Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:phpstan</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:phpstan</code></li>
                     <li><a href="/docs/managed-tests/phpcompatibility">PHPCompatibility Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:phpcompatibility</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:phpcompatibility</code></li>
                     <li><a href="/docs/managed-tests/malware">Malware Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:malware</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:malware</code></li>
+                    <li><a href="/docs/managed-tests/validation">Validation Tests</a> <code
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:validation</code></li>
                     <li>Performance Tests <i>(Coming soon)</i></li>
                 </ul>
             </li>
@@ -29,7 +31,7 @@ export default function TestTypes({ includeCode = false }) {
                 Custom Tests
                 <ul>
                     <li><a href="/docs/custom-tests/introduction">Custom E2E Tests</a> <code
-                        style={{display: includeCode ? 'inline-block' : 'none'}}>run:e2e</code></li>
+                        style={{ display: includeCode ? 'inline-block' : 'none' }}>run:e2e</code></li>
                 </ul>
             </li>
 
